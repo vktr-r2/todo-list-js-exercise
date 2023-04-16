2780db6a84ef5f0c0a2825ecfe134759fe417245
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-15 22:56:32 -0400

    Refactor functions to update and print object values

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function newTask (title, description) {
   return task;
 }
 
+// prints provided task details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task){
+  task.complete = true;
+}
+
+
+
 
 
 // DRIVER CODE BELOW
@@ -14,5 +26,10 @@ function newTask (title, description) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+
+logTaskState(task1);
+completeTask(task1);
+logTaskState(task1);
+
 
 console.log(tasks);
